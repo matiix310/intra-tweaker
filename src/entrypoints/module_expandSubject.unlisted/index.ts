481c9c738a927e4fad35dccf8f419eb4367d84a1
Milo Moisson <milo@wiro.world>
2025-10-21 23:06:25 +0200

    fix: expandSubject: fix typing

--- src/entrypoints/module_expandSubject.unlisted/index.ts
+++ src/entrypoints/module_expandSubject.unlisted/index.ts
@@ -1,9 +1,14 @@
 const run = async () => {
-  const isSubject = window.location.pathname.split("/").at(-1) == "subject.html";
+  const isSubject =
+    window.location.pathname.split("/").at(-1) == "subject.html";
   if (!isSubject) return;
 
-  // Expand all subject details
-  document.querySelectorAll('[aria-expanded=false]').forEach((v) => v.click());
+  let sections = document.querySelectorAll<HTMLButtonElement>(
+    "button[aria-expanded=false]",
+  );
+
+  // Expand all subject details but the guidelines
+  sections.forEach((v) => v.click());
 };
 
 export default defineUnlistedScript(() => {
